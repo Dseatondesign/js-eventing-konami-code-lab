b190fff7fadee7a0cd6ce360b5a0080f5e1adbb0
Dane <Dseatondesign@gmail.com>
2022-07-27 20:52:11 +1000

    completed assignment

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const alphabet = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0
 
-function init() {
-  // Write your JavaScript code inside the init() function
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === alphabet[index]) {
+      index++;
+      if (index === alphabet.length) {
+        alert("Congratulations you have entered the Konami Code correctly!");
+        index = 0;
+        console.log ("code worked")
+      }
+    } else {
+      index = 0;
+      console.log ("code failed")
+    }
+  }
 
-}
+  document.body.addEventListener("keydown", onKeyDownHandler)
